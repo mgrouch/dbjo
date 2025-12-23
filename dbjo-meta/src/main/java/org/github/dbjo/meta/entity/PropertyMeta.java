ea0888600ab133ceca26c8200308ff90ed5e26c8
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2025-12-23 15:19:55 -0500

    init

--- dbjo-meta/src/main/java/org/github/dbjo/meta/entity/PropertyMeta.java
+++ dbjo-meta/src/main/java/org/github/dbjo/meta/entity/PropertyMeta.java
@@ -3,10 +3,29 @@ package org.github.dbjo.meta.entity;
 import java.io.Serializable;
 
 public record PropertyMeta<B extends Serializable, V extends Serializable>(
-        String propId,
-        String propDesc,
-        Class<V> propClass,
-        PropAccessor<B, V> propAccessor
-) implements Serializable {
+        String propertyName,
+        Class<V> propertyClass,
+        PropGetter<B, V> getter,
+        PropSetter<B, V> setter
+) implements PropAccessor<B, V> {
 
+    @Override
+    public String getPropertyName() {
+        return propertyName;
+    }
+
+    @Override
+    public Class<V> getPropertyClass() {
+        return propertyClass;
+    }
+
+    @Override
+    public V get(B bean) {
+        return getter.get(bean);
+    }
+
+    @Override
+    public void set(B bean, V value) {
+        setter.set(bean, value);
+    }
 }
