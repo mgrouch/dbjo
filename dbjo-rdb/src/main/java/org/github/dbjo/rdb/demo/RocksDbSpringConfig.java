469152039aaf8a050fd9c2ab71ad4e2f599928bd
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-01-06 15:33:11 -0500

    init

---
+++ dbjo-rdb/src/main/java/org/github/dbjo/rdb/demo/RocksDbSpringConfig.java
@@ -0,0 +1,26 @@
+package org.github.dbjo.rdb.demo;
+
+import org.github.dbjo.rdb.*;
+import org.springframework.context.annotation.*;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration(proxyBeanMethods = false)
+@EnableTransactionManagement
+public class RocksDbSpringConfig {
+
+    @Bean
+    public PlatformTransactionManager transactionManager(RocksDbHandle h) {
+        return new RocksDbTransactionManager(h.db());
+    }
+
+    @Bean
+    public RocksSessions rocksSessions(RocksDbHandle h) {
+        return new SpringRocksSessions(h.db());
+    }
+
+    @Bean
+    public DaoRegistry daoRegistry(RocksDbHandle h) {
+        return new DaoRegistry(h.cfByName());
+    }
+}
