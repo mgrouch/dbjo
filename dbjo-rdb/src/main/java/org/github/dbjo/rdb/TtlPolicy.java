17ec4efddb9353f20190c01a4b80d10aab50231c
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-01-06 15:53:31 -0500

    init

--- dbjo-rdb/src/main/java/org/github/dbjo/rdb/TtlPolicy.java
+++
@@ -1,9 +0,0 @@
-package org.github.dbjo.rdb;
-
-import java.util.OptionalLong;
-
-public interface TtlPolicy<T> {
-    /** Return expiry epoch seconds, or empty for “no expiry”. */
-    OptionalLong expiresAtEpochSeconds(T value);
-}
-
