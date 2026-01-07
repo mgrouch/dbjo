3b80972c78bee35d1f4d92345a0385feb7d816e6
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-01-07 12:08:53 -0500

    init

--- dbjo-rdb/src/main/java/org/github/dbjo/rdb/demo/UserDao.java
+++ dbjo-rdb/src/main/java/org/github/dbjo/rdb/demo/UserDao.java
@@ -4,12 +4,6 @@ import org.github.dbjo.rdb.*;
 
 public final class UserDao extends IndexedRocksDao<User, String> {
     public UserDao(RocksSessions sessions, DaoRegistry registry, UserProtoMapper mapper) {
-        super(
-                sessions,
-                registry.entity(UserSchema.def(
-                        registry.cf(UserSchema.USERS_CF),
-                        mapper
-                ))
-        );
+        super(sessions, registry.entity(UserSchema.def(registry.cf(UserSchema.USERS_CF), mapper)));
     }
 }
