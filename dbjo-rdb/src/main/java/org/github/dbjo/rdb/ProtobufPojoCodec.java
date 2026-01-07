9f137ca768a6c1674305bc1f409c23deafebc0c8
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-01-07 11:22:12 -0500

    init

--- dbjo-rdb/src/main/java/org/github/dbjo/rdb/ProtobufPojoCodec.java
+++ dbjo-rdb/src/main/java/org/github/dbjo/rdb/ProtobufPojoCodec.java
@@ -2,6 +2,8 @@ package org.github.dbjo.rdb;
 
 import com.google.protobuf.MessageLite;
 
+import java.util.Objects;
+
 public final class ProtobufPojoCodec<P, M extends MessageLite> implements Codec<P> {
 
     public interface ProtoMapper<P, M extends MessageLite> {
@@ -9,19 +11,21 @@ public final class ProtobufPojoCodec<P, M extends MessageLite> implements Codec<
         P fromProto(M proto);
     }
 
-    private final ProtobufCodec<M> protoCodec;
+    private final Codec<M> protoCodec;          // <- was ProtobufCodec<M>
     private final ProtoMapper<P, M> mapper;
 
-    public ProtobufPojoCodec(ProtobufCodec<M> protoCodec, ProtoMapper<P, M> mapper) {
-        this.protoCodec = protoCodec;
-        this.mapper = mapper;
+    public ProtobufPojoCodec(Codec<M> protoCodec, ProtoMapper<P, M> mapper) {
+        this.protoCodec = Objects.requireNonNull(protoCodec);
+        this.mapper = Objects.requireNonNull(mapper);
     }
 
-    @Override public byte[] encode(P value) {
+    @Override
+    public byte[] encode(P value) {
         return protoCodec.encode(mapper.toProto(value));
     }
 
-    @Override public P decode(byte[] bytes) {
+    @Override
+    public P decode(byte[] bytes) {
         return mapper.fromProto(protoCodec.decode(bytes));
     }
 }
