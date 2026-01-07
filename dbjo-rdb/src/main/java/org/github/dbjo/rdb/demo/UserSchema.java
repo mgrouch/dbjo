6e4c0088f76bc6b629a681b9a2ed9aa4b32d5563
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-01-06 23:50:48 -0500

    init

--- dbjo-rdb/src/main/java/org/github/dbjo/rdb/demo/UserSchema.java
+++ dbjo-rdb/src/main/java/org/github/dbjo/rdb/demo/UserSchema.java
@@ -1,31 +1,8 @@
 package org.github.dbjo.rdb.demo;
 
-import org.github.dbjo.rdb.*;
-import org.rocksdb.ColumnFamilyHandle;
-
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-
 public final class UserSchema {
     public static final String USERS_CF = "users";
     public static final String IDX_EMAIL = "users_email_idx";
-
-    public static EntityDef<User, String> def(ColumnFamilyHandle usersCf, ColumnFamilyHandle emailIdxCf) {
-        // protobuf codec (your DAOs store protobuf bytes directly)
-        Codec<User> userCodec = ProtobufCodec.ofDefault(User.getDefaultInstance());
-
-        return new EntityDef<>(
-                USERS_CF,
-                usersCf,
-                KeyCodec.stringUtf8(),
-                userCodec,
-                List.of(
-                        IndexDef.unique(IDX_EMAIL, emailIdxCf,
-                                (User u) -> u.hasEmail() ? u.getEmail() : null
-                        )
-                )
-        );
-    }
 
     private UserSchema() {}
 }
