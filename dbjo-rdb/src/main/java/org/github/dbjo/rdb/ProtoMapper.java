c1646f3b29106a0ade91a90fe2e6ba3fc4ace35f
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-01-07 19:53:38 -0500

    init

--- dbjo-rdb/src/main/java/org/github/dbjo/rdb/ProtoMapper.java
+++
@@ -1,8 +0,0 @@
-package org.github.dbjo.rdb;
-
-import com.google.protobuf.MessageLite;
-
-public interface ProtoMapper<P, M extends MessageLite> {
-    M toProto(P pojo);
-    P fromProto(M msg);
-}
