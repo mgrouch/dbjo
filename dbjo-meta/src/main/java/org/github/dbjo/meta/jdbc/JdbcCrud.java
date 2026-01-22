0752dfec12977acebccbe33a7b88e82471da96c1
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-01-22 16:23:17 -0500

    jdbc API codegen

--- dbjo-meta/src/main/java/org/github/dbjo/meta/jdbc/JdbcCrud.java
+++ dbjo-meta/src/main/java/org/github/dbjo/meta/jdbc/JdbcCrud.java
@@ -1,21 +1,23 @@
 package org.github.dbjo.meta.jdbc;
 
+import java.sql.Connection;
+
 public final class JdbcCrud {
-    public static <T> void insert(java.sql.Connection c, DbMeta<T> m, T e) throws Exception {
+    public static <T> void insert(Connection c, DbMeta<T> m, T e) throws Exception {
         try (var ps = c.prepareStatement(m.insertSql())) {
             Jdbc.bind(ps, m.insertParams(e), m.insertParamTypes());
             ps.executeUpdate();
         }
     }
 
-    public static <T> void updateById(java.sql.Connection c, DbMeta<T> m, T e) throws Exception {
+    public static <T> void updateById(Connection c, DbMeta<T> m, T e) throws Exception {
         try (var ps = c.prepareStatement(m.updateByIdSql())) {
             Jdbc.bind(ps, m.updateByIdParams(e), m.updateByIdParamTypes());
             ps.executeUpdate();
         }
     }
 
-    public static <T> java.util.List<T> selectAll(java.sql.Connection c, DbMeta<T> m) throws Exception {
+    public static <T> java.util.List<T> selectAll(Connection c, DbMeta<T> m) throws Exception {
         try (var ps = c.prepareStatement(m.selectAllSql());
              var rs = ps.executeQuery()) {
             var out = new java.util.ArrayList<T>();
