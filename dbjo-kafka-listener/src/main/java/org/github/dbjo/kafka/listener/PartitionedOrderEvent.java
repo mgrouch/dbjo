0f481a5efc0174e8f50ad1e7641607a60f8679c6
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-02-10 15:45:02 -0500

    Refactor Kafka listener/publisher to generic partitioned APIs

--- dbjo-kafka-listener/src/main/java/org/github/dbjo/kafka/listener/PartitionedOrderEvent.java
+++ dbjo-kafka-listener/src/main/java/org/github/dbjo/kafka/listener/PartitionedOrderEvent.java
@@ -2,10 +2,12 @@ package org.github.dbjo.kafka.listener;
 
 import org.github.dbjo.kafka.avro.OrderEvent;
 
-public record PartitionedOrderEvent(int partition, long offset, String key, long timestamp, OrderEvent event) {
-    public PartitionedOrderEvent {
-        if (event == null) {
-            throw new IllegalArgumentException("event must not be null");
-        }
+public class PartitionedOrderEvent extends PartitionedKafkaEvent<OrderEvent> {
+    public PartitionedOrderEvent(int partition, long offset, String key, long timestamp, OrderEvent event) {
+        super(partition, offset, key, timestamp, event);
+    }
+
+    public String key() {
+        return getPartitionKey();
     }
 }
